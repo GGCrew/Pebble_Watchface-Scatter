78ecce6a25eabf01e4134f0a0a6c8918dd91a768
GGCrew <R_Connelie@hotmail.com>
2013-08-30 02:45:27 +0000

    Prepping for SDK2 version of scatter by running the following commands: ~/pebble-dev/pebble-sdk-2/bin/pb-sdk new-project SDK2_scatter cd SDK2_scatter ln -s /media/sf_pebble/SDK2_scatter/build/ build mv src src.local ln -s /media/sf_pebble/SDK2_scatter/src/ src cp src.local/* src/ cd resources mv src src.local ln -s /media/sf_pebble/SDK2_scatter/resources/src/ src cp src.local/* src/ cd ..

---
+++ src/SDK2_scatter.c
@@ -0,0 +1,56 @@
+#include <pebble_os.h>
+#include <pebble_app.h>
+#include <pebble_fonts.h>
+
+
+#define MY_UUID { 0x53, 0x4A, 0x0A, 0x06, 0x30, 0x1E, 0x4D, 0x7E, 0xBB, 0xE1, 0xEE, 0xA5, 0x84, 0x84, 0x52, 0xC5 }
+PBL_APP_INFO(MY_UUID,
+             "Template App", "Your Company",
+             1, 0, /* App version */
+             DEFAULT_MENU_ICON,
+             APP_INFO_STANDARD_APP);
+
+static Window *window;
+static TextLayer *text_layer;
+
+void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+}
+
+void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Up");
+}
+
+void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Down");
+}
+
+void config_provider(ClickConfig **config, Window *window) {
+  config[BUTTON_ID_SELECT]->click.handler = select_click_handler;
+  config[BUTTON_ID_UP]->click.handler = up_click_handler;
+  config[BUTTON_ID_DOWN]->click.handler = down_click_handler;
+}
+
+void handle_init(void) {
+  window = window_create();
+  window_stack_push(window, true /* Animated */);
+
+  window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
+
+  text_layer = text_layer_create(GRect(/* x: */ 0, /* y: */ 74,
+                                       /* width: */ 144, /* height: */ 20));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+  text_layer_set_text(text_layer, "Press a button");
+}
+
+void handle_deinit(void) {
+  text_layer_destroy(text_layer);
+  window_destroy(window);
+}
+
+int main(void) {
+  handle_init();
+  app_event_loop();
+  handle_deinit();
+}
